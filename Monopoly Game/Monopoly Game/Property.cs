9d50d6415dfb5910fb7af0ccd20075fc3eec2447
chrirex <43507156+chrirex@users.noreply.github.com>
2019-01-27 22:30:07 -0700

    Add files via upload

--- Monopoly Game/Monopoly Game/Property.cs
+++ Monopoly Game/Monopoly Game/Property.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Monopoly_Game
 {
+    /*
+         * Rex Christesnsen: Property class 1.0
+         * 
+         * Please describe changes made here; along with your name, date, and version:
+         * Addws public Constructor for use with Tic Tac Toe test harness - Rex Christensen - 27JAN1019 - v1
+         * 
+         */
     class Property : Space
     {
         public int value, owner;
@@ -14,6 +21,17 @@ namespace Monopoly_Game
             owner = -1;
             value = 0;
         }
+
+        /// <summary>
+        /// This constructor for use with the Tic Tac Toe test harness
+        /// Rex Christensen - 27JAN2019 - v1
+        /// </summary>
+        /// <param name="inputValue"></param>
+        public Property(int inputValue) {
+            owner = -1;
+            value = inputValue;
+        }
+
         public Property(int inputOwner, int inputValue)
         {
             owner = inputOwner;
