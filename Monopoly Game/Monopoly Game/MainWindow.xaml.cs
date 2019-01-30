8c6f2ce68b147e3e2e25ab5e238dde71d0670fbc
chrirex <43507156+chrirex@users.noreply.github.com>
2019-01-29 22:48:47 -0700

    Add files via upload
    Changed the MainWindow code behind to reflect a change from the LinkedList data structure to the List data structure in the board class.

--- Monopoly Game/Monopoly Game/MainWindow.xaml.cs
+++ Monopoly Game/Monopoly Game/MainWindow.xaml.cs
@@ -18,6 +18,15 @@ namespace Monopoly_Game
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    /*
+    * Rex Christesnsen: MainWindow.xaml.cs class 1.0
+    * 
+    * Please describe changes made here; along with your name, date, and version:
+    * Added a Tic Tac Toe board composed of 9 buttons that will show either X or O depending on what player clicked on them - R.C. - 27JAN19
+    * Adjusted the event handlers for each button to reflect change from LinkedList to List data structure - R.C. - 29JAN19 - v1
+    * 
+    */
     public partial class MainWindow : Window
     {
         TicTacToe game;
@@ -28,215 +37,161 @@ namespace Monopoly_Game
             game = new TicTacToe();
         }
 
-        private LinkedListNode<Space> getCurrentNode(int square){
-            playerID = game.CurrentPlayer.playerID;
+        private Property getProperty(int square) {
             Property prop = (Property)game.GameBoard.Spaces.ElementAt(square);
-
-            int i = 0;
-            LinkedListNode<Space> currentNode = game.GameBoard.Spaces.First;
-            while (i < square) {
-                currentNode = currentNode.Next;
-                i++;
-            }
-
-            return currentNode;
+            return prop;
         }
 
         private void BtnSquare6_Click(object sender, RoutedEventArgs e) {
             playerID = game.CurrentPlayer.playerID;
-            Property prop = (Property)game.GameBoard.Spaces.ElementAt(6);
-
-            LinkedListNode<Space> currentNode = getCurrentNode(6);
+            Property prop = getProperty(6);
 
             if (prop.owner == -1) {
                 prop.owner = playerID;
-                currentNode.Value = prop;
+                game.GameBoard.Spaces[6] = prop;
                 if (playerID == 0) {
                     tbSquare6.Text = "X";
                 } else {
                     tbSquare6.Text = "O";
                 }
                 game.makeNextPlayersTurn();
-                if (game.checkForTicTacToeWin())
-                {
-                    System.Windows.Application.Current.Shutdown();
-                }
+                game.checkForTicTacToeWin();
             }
         }
 
         private void BtnSquare7_Click(object sender, RoutedEventArgs e) {
             playerID = game.CurrentPlayer.playerID;
-            Property prop = (Property)game.GameBoard.Spaces.ElementAt(7);
-
-            LinkedListNode<Space> currentNode = getCurrentNode(7);
+            Property prop = getProperty(7);
 
             if (prop.owner == -1) {
                 prop.owner = playerID;
-                currentNode.Value = prop;
+                game.GameBoard.Spaces[7] = prop;
                 if (playerID == 0) {
                     tbSquare7.Text = "X";
                 } else {
                     tbSquare7.Text = "O";
                 }
                 game.makeNextPlayersTurn();
-                if (game.checkForTicTacToeWin())
-                {
-                    System.Windows.Application.Current.Shutdown();
-                }
+                game.checkForTicTacToeWin();
             }
         }
 
         private void BtnSquare8_Click(object sender, RoutedEventArgs e) {
             playerID = game.CurrentPlayer.playerID;
-            Property prop = (Property)game.GameBoard.Spaces.ElementAt(8);
-
-            LinkedListNode<Space> currentNode = getCurrentNode(8);
+            Property prop = getProperty(8);
 
             if (prop.owner == -1) {
                 prop.owner = playerID;
-                currentNode.Value = prop;
+                game.GameBoard.Spaces[8] = prop;
                 if (playerID == 0) {
                     tbSquare8.Text = "X";
                 } else {
                     tbSquare8.Text = "O";
                 }
                 game.makeNextPlayersTurn();
-                if (game.checkForTicTacToeWin())
-                {
-                    System.Windows.Application.Current.Shutdown();
-                }
+                game.checkForTicTacToeWin();
             }
         }
 
         private void BtnSquare3_Click(object sender, RoutedEventArgs e) {
             playerID = game.CurrentPlayer.playerID;
-            Property prop = (Property)game.GameBoard.Spaces.ElementAt(3);
-
-            LinkedListNode<Space> currentNode = getCurrentNode(3);
+            Property prop = getProperty(3); ;
 
             if (prop.owner == -1) {
                 prop.owner = playerID;
-                currentNode.Value = prop;
+                game.GameBoard.Spaces[3] = prop;
                 if (playerID == 0) {
                     tbSquare3.Text = "X";
                 } else {
                     tbSquare3.Text = "O";
                 }
                 game.makeNextPlayersTurn();
-                if (game.checkForTicTacToeWin())
-                {
-                    System.Windows.Application.Current.Shutdown();
-                }
+                game.checkForTicTacToeWin();
             }
         }
 
         private void BtnSquare4_Click(object sender, RoutedEventArgs e) {
             playerID = game.CurrentPlayer.playerID;
-            Property prop = (Property)game.GameBoard.Spaces.ElementAt(4);
-
-            LinkedListNode<Space> currentNode = getCurrentNode(4);
+            Property prop = getProperty(4);
 
             if (prop.owner == -1) {
                 prop.owner = playerID;
-                currentNode.Value = prop;
+                game.GameBoard.Spaces[4] = prop;
                 if (playerID == 0) {
                     tbSquare4.Text = "X";
                 } else {
                     tbSquare4.Text = "O";
                 }
                 game.makeNextPlayersTurn();
-                if (game.checkForTicTacToeWin())
-                {
-                    System.Windows.Application.Current.Shutdown();
-                }
+                game.checkForTicTacToeWin();
             }
         }
 
         private void BtnSquare5_Click(object sender, RoutedEventArgs e) {
             playerID = game.CurrentPlayer.playerID;
-            Property prop = (Property)game.GameBoard.Spaces.ElementAt(5);
-
-            LinkedListNode<Space> currentNode = getCurrentNode(5);
+            Property prop = getProperty(5);
 
             if (prop.owner == -1) {
                 prop.owner = playerID;
-                currentNode.Value = prop;
+                game.GameBoard.Spaces[5] = prop;
                 if (playerID == 0) {
                     tbSquare5.Text = "X";
                 } else {
                     tbSquare5.Text = "O";
                 }
                 game.makeNextPlayersTurn();
-                if (game.checkForTicTacToeWin())
-                {
-                    System.Windows.Application.Current.Shutdown();
-                }
+                game.checkForTicTacToeWin();
             }
         }
 
         private void BtnSquare0_Click(object sender, RoutedEventArgs e) {
             playerID = game.CurrentPlayer.playerID;
-            Property prop = (Property)game.GameBoard.Spaces.ElementAt(0);
-
-            LinkedListNode<Space> currentNode = game.GameBoard.Spaces.First;
+            Property prop = getProperty(0);
 
             if (prop.owner == -1) {
                 prop.owner = playerID;
-                currentNode.Value = prop;
+                game.GameBoard.Spaces[0] = prop;
                 if (playerID == 0) {
                     tbSquare0.Text = "X";
                 } else {
                     tbSquare0.Text = "O";
                 }
                 game.makeNextPlayersTurn();
-                if (game.checkForTicTacToeWin())
-                {
-                    System.Windows.Application.Current.Shutdown();
-                }
+                game.checkForTicTacToeWin();
             }
         }
 
         private void BtnSquare1_Click(object sender, RoutedEventArgs e) {
             playerID = game.CurrentPlayer.playerID;
-            Property prop = (Property)game.GameBoard.Spaces.ElementAt(1);
-
-            LinkedListNode<Space> currentNode = getCurrentNode(1);
+            Property prop = getProperty(1);
 
             if (prop.owner == -1) {
                 prop.owner = playerID;
-                currentNode.Value = prop;
+                game.GameBoard.Spaces[1] = prop;
                 if (playerID == 0) {
                     tbSquare1.Text = "X";
                 } else {
                     tbSquare1.Text = "O";
                 }
                 game.makeNextPlayersTurn();
-                if (game.checkForTicTacToeWin())
-                {
-                    System.Windows.Application.Current.Shutdown();
-                }
+                game.checkForTicTacToeWin();
             }
         }
 
         private void BtnSquare2_Click(object sender, RoutedEventArgs e) {
             playerID = game.CurrentPlayer.playerID;
-            Property prop = (Property)game.GameBoard.Spaces.ElementAt(2);
-
-            LinkedListNode<Space> currentNode = getCurrentNode(2);
+            Property prop = getProperty(2);
 
             if (prop.owner == -1) {
                 prop.owner = playerID;
-                currentNode.Value = prop;
+                game.GameBoard.Spaces[2] = prop;
                 if (playerID == 0) {
                     tbSquare2.Text = "X";
                 } else {
                     tbSquare2.Text = "O";
                 }
                 game.makeNextPlayersTurn();
-                if (game.checkForTicTacToeWin())
-                {
-                    System.Windows.Application.Current.Shutdown();
-                }
+                game.checkForTicTacToeWin();
             }
         }
     }
--- Monopoly Game/Monopoly Game/TicTacToe.cs
+++ Monopoly Game/Monopoly Game/TicTacToe.cs
@@ -11,6 +11,7 @@ namespace Monopoly_Game
      * 
      * Please describe changes made here; along with your name, date, and version:
      * Created public Constructor - Rex Christensen - 27JAN1019 - v1
+     * Adjusted the adding of new properties to reflect the List data structure instead of the LinkedList - R.C. - 29JAN19 - v1
      * 
      */
     class TicTacToe : Game
@@ -18,7 +19,18 @@ namespace Monopoly_Game
         public TicTacToe()
         {
             for (int i = 0; i < 9; i++)
-                base.GameBoard.Spaces.AddLast(new Property(i));// For tictactoe, owner is a needed variable, so type property is used
-        }   
+                base.GameBoard.Spaces.Add(new Property(i));// For tictactoe, owner is a needed variable, so type property is used
+        }
+
+        public bool checkForTicTacToeWin() {
+            bool win = false;
+            // Check for horizontal wins
+
+            // Check for verticle wins
+
+            // Check for diagonal wins
+
+            return win;
+        }
     }
 }
