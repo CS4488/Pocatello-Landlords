f0a1fb1c4fa31a2c3b60bb9ba7d86240ae60be3d
Michael Sterner <46682781+stermic2@users.noreply.github.com>
2019-01-30 08:11:11 -0700

    Fernando's method was moved
    The method for ending the tic tac toe game was moved to the Tic Tac Toe Class.

--- Monopoly Game/Monopoly Game/Game.cs
+++ Monopoly Game/Monopoly Game/Game.cs
@@ -70,50 +70,6 @@ namespace Monopoly_Game
            }
         }
 
-        /*
-         * Author: Fernando Munoz
-         * Date: January 28th, 2019
-         * Method: Win Condition Check
-         * Version: 1.0
-         * 
-         * Description: Checks the gameBoard spaces for a 3-in-a-row match. Returns true if win condition found. 
-         *              Will also end program. We might want to change this. :P
-         */ 
-        public bool checkForTicTacToeWin() {
-
-            // Temporary cast while the list is changed to an array
-            Property[] temp = this.gameBoard.Spaces.Cast<Property>().ToArray();
-
-           // Horizontal checks
-            for(int i = 0; i < 8; i += 3)
-            {
-                if( temp[i].owner == temp[i + 1].owner && temp[i].owner == temp[i + 2].owner && temp[i].owner != -1)
-                {
-                    return true;
-                }
-            }
-
-            // Vertical checks
-            for (int i = 0; i < 3; i++)
-            {
-                if (temp[i].owner == temp[i + 3].owner && temp[i].owner == temp[i + 6].owner && temp[i].owner != -1)
-                {
-                    return true;
-                }
-            }
-            
-            // Diagonal checks
-            if (temp[0].owner == temp[4].owner && temp[0].owner == temp[8].owner && temp[0].owner != -1)
-            {
-                return true;
-            }
-
-            if (temp[2].owner == temp[4].owner && temp[2].owner == temp[6].owner && temp[2].owner != -1)
-            {
-                return true;
-            }
-
-            return false;
-        }
+        
     }
 }
