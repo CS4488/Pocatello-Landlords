49f0f53b571b81f24eec3fbea96218dccd2318cf
Fernando Munoz <munozfernando92@yahoo.com>
2019-03-12 18:30:59 -0600

    lots of UI changes and code to get all textboxes for prices, names and buttons

--- Monopoly Game/Monopoly Game/MainWindow.xaml.cs
+++ Monopoly Game/Monopoly Game/MainWindow.xaml.cs
@@ -31,96 +31,98 @@ namespace Monopoly_Game
     */
     public partial class MainWindow : Window
     {
-        TicTacToe game;
-        DisplayManager dm;
+        //    TicTacToe game;
+        //    DisplayManager dm;
 
-        Dictionary<Tuple<int, int>, int> gridToIndexMap = new Dictionary<Tuple<int, int>, int>();
+        //    Dictionary<Tuple<int, int>, int> gridToIndexMap = new Dictionary<Tuple<int, int>, int>();
 
         public MainWindow()
         {
             InitializeComponent();
-            playArea.Visibility = Visibility.Hidden;
-            playArea.IsEnabled = false;
-            fillMap();
+            //playArea.Visibility = Visibility.Hidden;
+            //playArea.IsEnabled = false;
+            //fillMap();
 
-            LandlordsBoard thing = new LandlordsBoard();
+            //LandlordsBoard thing = new LandlordsBoard();
+            frm_Main.Content = new LandlordsBoard();
         }
 
-        //handle all button clicks
-        private void BtnClick(object sender, RoutedEventArgs e)
-        {
-            Button btn = sender as Button;
-            int x = (int)btn.GetValue(Grid.RowProperty);
-            int y = (int)btn.GetValue(Grid.ColumnProperty);
+        //    //handle all button clicks
+        //    private void BtnClick(object sender, RoutedEventArgs e)
+        //    {
+        //        Button btn = sender as Button;
+        //        int x = (int)btn.GetValue(Grid.RowProperty);
+        //        int y = (int)btn.GetValue(Grid.ColumnProperty);
 
-            Tuple<int, int> coords = Tuple.Create(x, y);
+        //        Tuple<int, int> coords = Tuple.Create(x, y);
 
-            int index = gridToIndexMap[coords];
-            handleGame(index);
-        }
+        //        int index = gridToIndexMap[coords];
+        //        handleGame(index);
+        //    }
 
-        // begin a new game
-        private void MiNewGame_Click(object sender, RoutedEventArgs e) {
-            game = new TicTacToe();
-            this.dm = new DisplayManager(game, playArea);
-            dm.updateDisplay();
-        }
+        //    // begin a new game
+        //    private void MiNewGame_Click(object sender, RoutedEventArgs e) {
+        //        game = new TicTacToe();
+        //        this.dm = new DisplayManager(game, playArea);
+        //        dm.updateDisplay();
+        //    }
 
-        private void MiJoinGame_Click(object sender, RoutedEventArgs e) {
-            MessageBox.Show("Functionality coming soon!");
-        }
+        //    private void MiJoinGame_Click(object sender, RoutedEventArgs e) {
+        //        MessageBox.Show("Functionality coming soon!");
+        //    }
 
-        private void MiObserveGame_Click(object sender, RoutedEventArgs e) {
-            MessageBox.Show("Functionality coming soon!");
-        }
+        //    private void MiObserveGame_Click(object sender, RoutedEventArgs e) {
+        //        MessageBox.Show("Functionality coming soon!");
+        //    }
 
-        private void MiExit_Click(object sender, RoutedEventArgs e) {
-            System.Windows.Application.Current.Shutdown();
-            return;
-        }
+        //    private void MiExit_Click(object sender, RoutedEventArgs e) {
+        //        System.Windows.Application.Current.Shutdown();
+        //        return;
+        //    }
 
-        //Dispatches game logic and display logic to proper classes
-        private void handleGame(int indexClicked)
-        {
-            game.handleTurn(indexClicked);
-            dm.updateDisplay();
-        }
+        //    //Dispatches game logic and display logic to proper classes
+        //    private void handleGame(int indexClicked)
+        //    {
+        //        game.handleTurn(indexClicked);
+        //        dm.updateDisplay();
+        //    }
 
-        private void fillMap()
-        {
-            //set up mapping
-            //definitely a way to do this mathematically
-            //but I wasn't sure how and this works
-            // formula is:
-            // x = index % col
-            // y = index / col
-            // if someone wants to reverse that
-            Tuple<int, int> coords = Tuple.Create(0, 0);
-            gridToIndexMap.Add(coords, 0);
+        //    private void fillMap()
+        //    {
+        //        //set up mapping
+        //        //definitely a way to do this mathematically
+        //        //but I wasn't sure how and this works
+        //        // formula is:
+        //        // x = index % col
+        //        // y = index / col
+        //        // if someone wants to reverse that
+        //        Tuple<int, int> coords = Tuple.Create(0, 0);
+        //        gridToIndexMap.Add(coords, 0);
 
-            coords = Tuple.Create(0, 1);
-            gridToIndexMap.Add(coords, 1);
-            
-            coords = Tuple.Create(0, 2);
-            gridToIndexMap.Add(coords, 2);
-            
-            coords = Tuple.Create(1, 0);
-            gridToIndexMap.Add(coords, 3);
-            
-            coords = Tuple.Create(1, 1);
-            gridToIndexMap.Add(coords, 4);
-            
-            coords = Tuple.Create(1, 2);
-            gridToIndexMap.Add(coords, 5);
-            
-            coords = Tuple.Create(2, 0);
-            gridToIndexMap.Add(coords, 6);
-            
-            coords = Tuple.Create(2, 1);
-            gridToIndexMap.Add(coords, 7);
-            
-            coords = Tuple.Create(2, 2);
-            gridToIndexMap.Add(coords, 8);
-        }
+        //        coords = Tuple.Create(0, 1);
+        //        gridToIndexMap.Add(coords, 1);
+
+        //        coords = Tuple.Create(0, 2);
+        //        gridToIndexMap.Add(coords, 2);
+
+        //        coords = Tuple.Create(1, 0);
+        //        gridToIndexMap.Add(coords, 3);
+
+        //        coords = Tuple.Create(1, 1);
+        //        gridToIndexMap.Add(coords, 4);
+
+        //        coords = Tuple.Create(1, 2);
+        //        gridToIndexMap.Add(coords, 5);
+
+        //        coords = Tuple.Create(2, 0);
+        //        gridToIndexMap.Add(coords, 6);
+
+        //        coords = Tuple.Create(2, 1);
+        //        gridToIndexMap.Add(coords, 7);
+
+        //        coords = Tuple.Create(2, 2);
+        //        gridToIndexMap.Add(coords, 8);
+        //    }
+        //}
     }
 }
